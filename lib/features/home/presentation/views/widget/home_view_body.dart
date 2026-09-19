1e0698d6989993ba98184c4360cccacb5f5bdecf
Ibrahim Mohamed <ibrahimmksa05@gmail.com>
2026-09-19 09:39:17 +0300

    Add styles for medium title text and update HomeViewBody layout

--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widget/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +9,17 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 16,
       children: [
         CustomAppBar(),
         SizedBox(
           height: MediaQuery.of(context).size.height * .3,
           child: FeaturedBooksListView(),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Text('Best Seller', style: Styles.titleMedium),
         ),
       ],
     );
